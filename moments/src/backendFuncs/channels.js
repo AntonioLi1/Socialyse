a8d05eb661f047c4b6bc1508e2b14eeb339bc786
Daniel Bui <danielngn5@gmail.com>
2022-11-13 18:53:27 +1100

    Started check into channel function, trying to test

--- moments/src/backendFuncs/channels.js
+++ moments/src/backendFuncs/channels.js
@@ -1,15 +1,47 @@
+import { firebaseConfig } from './data.js';
 import database from '@react-native-firebase/database';
 
-function CheckInChannel(token, channelId) {
+// Function to check a user into a channel using the channel id
+// To check in a user is required to make a post in the channel
+// Everytime a user checks in the number of active users should be updated
+function CheckInChannel(channelId, caption, image, username) {
+    const db = database();
 
+    const post_info = {
+        "caption": caption,
+        "image": image,
+        "time": firebase.database.ServerValue.TIMESTAMP,
+        "username": username
+    }
+    
+    // Push data to the reference for channel posts
+    const newChannelRef = db.ref('channels' + channelId + '/posts').push();
+
+    // Print out the generated key
+    console.log('Auto generated key: ', newChannelRef.key);
+
+    newChannelRef
+        .set({post_info})
+        .then(() => console.log('Data updated'));
 }
+
 // timer???
-function CheckOutChannel(token, channelId, timer, userLongitude, userLatitude) {
+// Function to check a user out of a channel using channel if
+function CheckOutChannel(token, channelId, timer) {
 
 }
+
 // timer???
 async function ViewChannelFeed(token, timer) {
 
 }
+
+// Make a new post in the channel to update the timer 
+// and be able to see new posts
+function NewChannelPost() {
+
+}
+
+CheckInChannel(1, 'lols', 'lols', 'newperson');
 
 export { CheckInChannel, CheckOutChannel, ViewChannelFeed }
--- moments/src/backendFuncs/data.js
+++ moments/src/backendFuncs/data.js
@@ -36,65 +36,65 @@ export const storage = getStorage(app);
 
 
 
-// Function to create a new user with basic information
-function create_user(email, name, username) {
-  const db = getDatabase();
+// // Function to create a new user with basic information
+// function create_user(email, name, username) {
+//   const db = getDatabase();
 
-  const user_info = {
-    "email": email,
-    "name": name, 
-    "username": username
-  }
+//   const user_info = {
+//     "email": email,
+//     "name": name, 
+//     "username": username
+//   }
 
-  // Get a key for a new user.
-  const new_user_key = push(child(ref(db), 'users')).key;
+//   // Get a key for a new user.
+//   const new_user_key = push(child(ref(db), 'users')).key;
 
-  // Write the new post's data simultaneously in the posts list and the user's post list.
-  const updates = {};
-  updates['/users/' + new_user_key] = user_info;
+//   // Write the new post's data simultaneously in the posts list and the user's post list.
+//   const updates = {};
+//   updates['/users/' + new_user_key] = user_info;
 
-  return update(ref(db), updates);
-}
+//   return update(ref(db), updates);
+// }
 
-// 
-function create_posts(caption, image, time, username, channel_name) {
-  const db = getDatabase();
+// // 
+// function create_posts(caption, image, time, username, channel_name) {
+//   const db = getDatabase();
 
-  const post_info = {
-    "caption": caption,
-    "image": image,
-    "time": time,
-    "username": username
-  }
+//   const post_info = {
+//     "caption": caption,
+//     "image": image,
+//     "time": time,
+//     "username": username
+//   }
 
-  // Get a key for a new post.
-  const new_post_key = push(child(ref(db), 'posts')).key;
+//   // Get a key for a new post.
+//   const new_post_key = push(child(ref(db), 'posts')).key;
 
-  // Write the new post's data simultaneously in the posts list and the user's post list.
-  const updates = {};
-  updates["/channels/" + channel_name + "/posts/" + new_post_key] = post_info;
+//   // Write the new post's data simultaneously in the posts list and the user's post list.
+//   const updates = {};
+//   updates["/channels/" + channel_name + "/posts/" + new_post_key] = post_info;
 
-  return update(ref(db), updates);
-}
+//   return update(ref(db), updates);
+// }
 
-// Functions to update specific user information
-function update_user_info(user_id, email, name, username) {
-  const db = getDatabase();
+// // Functions to update specific user information
+// function update_user_info(user_id, email, name, username) {
+//   const db = getDatabase();
 
-  update(ref(db, "/users/" + user_id), {
-    "email": email,
-    "name": name, 
-    "username": username
-  });
-}
+//   update(ref(db, "/users/" + user_id), {
+//     "email": email,
+//     "name": name, 
+//     "username": username
+//   });
+// }
 
-// Order the list of posts by time
-function list_posts(channel_name) {
-  const db = getDatabase();
+// // Order the list of posts by time
+// function list_posts(channel_name) {
+//   const db = getDatabase();
 
-  const posts_by_time = query(ref(db, "channels/" + channel_name + "/posts/"), orderByChild("/time"));
+//   const posts_by_time = query(ref(db, "channels/" + channel_name + "/posts/"), orderByChild("/time"));
 
-  console.log(posts_by_time);
-}
+//   console.log(posts_by_time);
+// }
 
 // list_posts("test3")
