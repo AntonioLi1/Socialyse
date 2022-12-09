d15d5427f684c2dbee63d682b56d20701084442f
AntonioLi <li.antonio2003@gmail.com>
2022-12-09 23:06:48 +1100

    added CRUD templates

--- moments/src/notifications.js
+++ moments/src/notifications.js
@@ -1,4 +1,4 @@
-import { StyleSheet, Text, View, SafeAreaView, Pressable, ScrollView } from 'react-native'
+import { StyleSheet, Text, View, SafeAreaView, Pressable, ScrollView, TextInput } from 'react-native'
 import React, {useContext, useState, useEffect}from 'react';
 import {GettingStartedContext} from '../App'
 
@@ -15,17 +15,63 @@ import ADIcon from 'react-native-vector-icons/AntDesign'
 import { ContinousBaseGesture } from 'react-native-gesture-handler/lib/typescript/handlers/gestures/gesture';
 
 
-const App2 = () => {
+const NotificationDisplay = () => {
 	const {messageDisplay, setMessageDisplay, notifDisplay, setNotifDisplay} = useContext(GettingStartedContext);
 
 	const [notifData, setnotifData] = useState(null)
+	const [loading, setLoading] = useState(false)
+	const [input, setInput] = useState('')
 	
 	const fetchData = async () => {
+
+		setLoading(true);
 
 		let {error, data} = await supabase
 		.from('testChannel')
 		.select()
-	
+		
+		setLoading(false);
+
+		if(error) 
+		{
+			console.log('gg', error.message)
+			return false
+		}
+		
+		if(data) {
+			
+			return data
+		}
+	}
+
+	useEffect(() => {
+
+		// INITIALIZE
+		
+		init ()
+		
+		console.log(notifData)
+	}, [])
+
+
+	const init = async () =>
+	{
+		
+		const result = await fetchData()
+		//setLoading(true);
+		setnotifData(result)
+		console.log("result", result)
+	}
+
+	//if (loading) return null;
+
+	// pushing data
+	const pushItem = async (parameter, param2) => {
+		const { data, error } = await supabase
+		.from('testChannel')
+		.insert([
+			{ name: parameter, lastname: param2},
+		])
 		if(error) 
 		{
 			console.log('gg', error.message)
@@ -37,22 +83,24 @@ const App2 = () => {
 		}
 	}
 
-	useEffect(() => {
-
-		// INITIALIZE
-		init ()
-		console.log(notifData)
-	}, [])
-
-
-	const init = async () =>
-	{
-		const result = await fetchData()
-		setnotifData(result)
+	// deleting data
+	const deleteItem = async() => {
+		const { data, error } = await supabase
+		.from('testChannel')
+		.delete()
+		.eq('id', 4)
 	}
-	
-  return (
-	<SafeAreaView style={styles.notifFullScreen}>   
+
+	// update data
+	const updateData = async() => {
+		const { data, error } = await supabase
+		.from('testChannel')
+		.update({ name: "testies" })
+		.eq('id', '2')
+	}
+
+	return (
+		<SafeAreaView style={styles.notifFullScreen}>   
 			<View style={styles.notifHeader}>
 				<View style={styles.notifInnerHeaderContainer}>
 					<Pressable style={styles.notifBackButton} onPress={() => {setMessageDisplay(true); navigation.goBack(); setNotifDisplay(true) }}>
@@ -67,15 +115,17 @@ const App2 = () => {
 
 			<View style={styles.notificationsList}>
 
-				<Text>
-					
+			<Pressable onPress={() => {updateData()}}>
+				<Text style={styles.captionDone}>
+					Done
 				</Text>
+			</Pressable>
 
 				
 			</View>	
 		</SafeAreaView>
-  )
+	)
 }
 
-export default App2
+export default NotificationDisplay
 
